2c4a953ebdac9af80e334813381fa230f31130da
Lucas Pires <lucaspiressimao@gmail.com>
2020-04-02 00:01:16 -0300

    Ajustando menu

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,12 @@
 function toggle(id){
+    var artcls = document.getElementsByTagName('article')
+    for (let i = 0; i < artcls.length; i++) {
+        const element = artcls[i];
+        element.style.display = 'none'
+    }
     var disp = document.getElementById(id).style.display
     if(disp == 'none')
-        document.getElementById(id).style.display = 'block'
+        document.getElementById(id).style.display = 'flex'
     else 
         document.getElementById(id).style.display = 'none'
 }
